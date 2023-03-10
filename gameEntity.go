7e0496bbb6cddf47eded49f7127ffb8899af0d0c
Natasha <75773163+natty-light@users.noreply.github.com>
2023-03-10 16:14:36 -0500

    Add comments and initial colision func def

--- gameEntity.go
+++ gameEntity.go
@@ -24,10 +24,12 @@ type EntityFlags struct {
 	whoami     int
 }
 
+// Getter function to return the glm.Mat4 translation matrix given the entities position
 func (entity *GameEntity) GetTransformation() glm.Mat4 {
 	return glm.Translate3D(entity.position[0], entity.position[1], 0.0)
 }
 
+// Helper function for creating the GameEntity pointer as well as the VAO and VBO for OpenGL
 func CreateGameEntity(position [2]float32, dimensions [2]float32, color glm.Vec3, vertices []float32, velocity [2]float32, whoami int) *GameEntity {
 	// Create openGL VAO and VBO, function found in main.go
 	vao, vbo := CreateVAO(vertices)
@@ -38,6 +40,7 @@ func CreateGameEntity(position [2]float32, dimensions [2]float32, color glm.Vec3
 	return entity
 }
 
+// Helper function to delete VAO and VBO objects before quitting execution
 func CleanUpEntity(entity *GameEntity) {
 	VAO, VBO := entity.vao, entity.vbo
 	gl.DeleteVertexArrays(1, &VAO)
@@ -60,6 +63,9 @@ func (entity *GameEntity) UpdatePosition(maxX, maxY int) {
 	}
 }
 
+// Takes a set of four vertices describing the shape of a tetrahedral game object,
+// as well as the objects position, velocity, and shape, and returns a
+// GameEntity pointer for that object
 func prepareSingleGameEntity(
 	PosPosVertex []float32,
 	PosNegVertex []float32,
@@ -83,6 +89,10 @@ func prepareSingleGameEntity(
 	return entity
 }
 
+// This function handles drawing a given gameEntity using the given shader program
+// It expects the precense of the gl interface, which in the case of this program,
+// is declared in the main package in main.go. Since this function is part of the same package,
+// we need not pass in the gl object
 func drawEntity(entity *GameEntity, shaderProgram uint32) {
 	transformation := entity.GetTransformation()
 	var objectColorLocation = gl.GetUniformLocation(shaderProgram, gl.Str("objectColor\x00"))
@@ -95,4 +105,13 @@ func drawEntity(entity *GameEntity, shaderProgram uint32) {
 	gl.BindVertexArray(entity.vao)                                // bind data
 	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(entity.vertices)/3)) // perform draw call
 	gl.BindVertexArray(0)                                         // unbind data (so we don't mistakenly use/modify it)
+}
+
+// TODO: Have this function check which side of the static entity the dyanmic
+// entity overlaps with, then adjust the collision accordingly
+// The static refers to the entity which we consider immovable for the purpose of
+// the collision, even if it is capabale of moving
+// For example, in a paddle-ball collision, consider the paddle static
+func checkEntityCollision(staticEntity *GameEntity, dynamicEntity *GameEntity) {
+
 }
